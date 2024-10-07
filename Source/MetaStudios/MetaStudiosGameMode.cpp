07d3ee84a33a29a001d139dcf0ab578c62e5def1
carttman <93562297+carttman@users.noreply.github.com>
2024-10-07 15:42:51 +0900

    setup

---
+++ Source/MetaStudios/MetaStudiosGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MetaStudiosGameMode.h"
+#include "MetaStudiosCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMetaStudiosGameMode::AMetaStudiosGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/MetaStudios/MetaStudiosGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MetaStudiosGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMetaStudiosGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMetaStudiosGameMode();
+};
+
+
+
