c8c3cc42489fbc67e6f914c4f6f513b49d17f505
carttman <93562297+carttman@users.noreply.github.com>
2024-10-21 21:16:30 +0900

    메인 맵 생성

---
+++ Source/MetaStudios/CHJ/MainPlatformGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainPlatformGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METASTUDIOS_API AMainPlatformGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
