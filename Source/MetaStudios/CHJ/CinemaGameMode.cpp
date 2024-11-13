95803fa63d09be1648d93861f35e2d8da6e43ae3
carttman <93562297+carttman@users.noreply.github.com>
2024-11-13 16:46:15 +0900

    세션 위젯 리팩토링 1, 시네마 게임모드 수정

--- Source/MetaStudios/CHJ/CinemaGameMode.cpp
+++ Source/MetaStudios/CHJ/CinemaGameMode.cpp
@@ -9,16 +9,14 @@
 APawn* ACinemaGameMode::SpawnDefaultPawnFor_Implementation(AController* NewPlayer, AActor* StartSpot)
 {
 	UE_LOG(LogTemp, Warning, TEXT("this Controller is : %s"), *NewPlayer->GetName());
-	return Super::SpawnDefaultPawnFor_Implementation(NewPlayer, StartSpot);
-
+	//return Super::SpawnDefaultPawnFor_Implementation(NewPlayer, StartSpot);
 	
-	// if(NewPlayer->IsLocalController())
-	// {
-	// 	return GetWorld()->SpawnActor<AJSH_Player>(ServerPlayer, StartSpot->GetActorLocation(), StartSpot->GetActorRotation());
-	// }
-	// else
-	// {
-	// 	return GetWorld()->SpawnActor<AMetaStudiosCharacter>(ClientPlayer, StartSpot->GetActorLocation(), StartSpot->GetActorRotation());
-	// }
-	//return nullptr;
+	if(NewPlayer->IsLocalController())
+	{
+		return GetWorld()->SpawnActor<AJSH_Player>(ServerPlayer, StartSpot->GetActorLocation(), StartSpot->GetActorRotation());
+	}
+	else
+	{
+		return GetWorld()->SpawnActor<AMetaStudiosCharacter>(ClientPlayer, StartSpot->GetActorLocation(), StartSpot->GetActorRotation());
+	}
 }
